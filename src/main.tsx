21985bfaffc549965d30d8fc81be661be4e71a42
paulasevero <paulinhasevero13@gmail.com>
2018-11-13 21:26:37 -0200

    set up firebase and firebase auth

--- src/main.tsx
+++ src/main.tsx
@@ -6,3 +6,21 @@ import { actions, state, view } from '@app/app'
 const main = app<State, Actions>(state, actions, view, document.body)
 
 const unsubscribe = location.subscribe(main.location)
+
+// Firebase App is always required and must be first
+import firebase from 'firebase/app'
+
+// Add additional services that you want to use
+import 'firebase/auth'
+
+const config = {
+  apiKey: 'AIzaSyABP-KuAtp-3AJnBl7qkVCO624vIBEPbqU',
+  authDomain: 'outfit-perfeito.firebaseapp.com',
+  databaseURL: 'https://outfit-perfeito.firebaseio.com',
+  projectId: 'outfit-perfeito',
+  storageBucket: 'outfit-perfeito.appspot.com',
+  messagingSenderId: '584371021851'
+}
+firebase.initializeApp(config)
+
+const firebaseAuthProvider = new firebase.auth.GoogleAuthProvider()
